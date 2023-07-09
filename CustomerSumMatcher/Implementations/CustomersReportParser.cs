284b84cc512a5e72a85680beb0aa04db3d2314e3
Piotr Sidor <piotr.sid14@gmail.com>
2023-07-09 10:16:16 +0200

    feat(parsers): implementation of CustomersReportParser and ReportItemsParser

---
+++ CustomerSumMatcher/Implementations/CustomersReportParser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using CustomerSumMatcher.Interfaces;
+
+namespace CustomerSumMatcher.Implementations
+{
+    public class CustomersReportParser : ICustomersReportParser
+    {
+        public IEnumerable<(string CustomerNumber, decimal Total)> Parse(string filePath)
+        {
+            using var reader = new StreamReader(filePath);
+            while (reader.ReadLine() is { } line)
+            {
+                var values = line.Split(',');
+                if (values.Length != 2 || !decimal.TryParse(values[0], out var total)) continue;
+                
+                yield return (values[1], total);
+            }
+        }
+    }
+}
---
+++ CustomerSumMatcher/Implementations/ReportItemsParser.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using CustomerSumMatcher.Interfaces;
+using CustomerSumMatcher.Model;
+
+namespace CustomerSumMatcher.Implementations
+{
+    public class ReportItemsParser : IReportItemsParser
+    {
+        public IEnumerable<PartialReportItem> Parse(string filePath)
+        {
+            using var reader = new StreamReader(filePath);
+            while (reader.ReadLine() is { } line)
+            {
+                var values = line.Split(',');
+
+                if (values.Length != 3 || !decimal.TryParse(values[0], out var amount)) continue;
+                yield return new PartialReportItem
+                {
+                    Amount = amount,
+                    CustomerNumber = values[1],
+                    Reference = values[2]
+                };
+            }
+        }
+    }
+}
